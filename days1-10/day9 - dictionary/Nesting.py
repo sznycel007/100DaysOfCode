b38e544519ea3ece7d9522d38d4a2fc1cb76505c
sg0306631 <karol.wojnar@sabre.com>
2022-12-12 10:39:02 +0100

    day 9 - push chapter project to repo

--- days1-10/day9 - dictionary/Nesting.py
+++ days1-10/day9 - dictionary/Nesting.py
@@ -17,3 +17,36 @@ print(travel_log["Germany"])
 
 # Nesting Dictionary in a Dictionary
 
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+print(travel_log)
+
+
+SpotifyWrap = {
+    "Band": {"Pink Floyd": ["Animals", "High Hopes", "Wish you where here"],"total_hours": 23}
+}
+print(SpotifyWrap)
+
+# Nesting dictionary in a list
+print()
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
+]
+print(travel_log[0])
+print(travel_log[1])
+
+
+
+
